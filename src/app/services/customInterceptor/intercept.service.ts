eaab074f5d60262c338a7dc206e2d322adea6f1a
milan760 <milan.sict@gmail.com>
2024-11-04 10:52:02 +0530

    Auto logout and redirect to signin page if token is expired

--- src/app/services/customInterceptor/intercept.service.ts
+++ src/app/services/customInterceptor/intercept.service.ts
@@ -4,6 +4,7 @@ import { Router } from "@angular/router";
 import { JwtHelperService } from "@auth0/angular-jwt";
 import { catchError, concatMap, delay, map, of, retryWhen, tap, throwError } from "rxjs";
 import { AuthService } from "../authentication/auth.service";
+import { StorageService } from "../storage/storage.service";
 
 export const retryCount = 1;
 export const retryDelayMS = 2000;
@@ -54,12 +55,13 @@ export const errorMsgs = {
 
 export const InterceptService: HttpInterceptorFn = (req, next) => {
   const jwtService = inject(JwtHelperService);
+  const storageService = inject(StorageService);
   const router = inject(Router);
   const authService = inject(AuthService);
   let token: any;
 
   try {
-    token = localStorage.getItem('accessTokenDetails');
+    token = storageService.getLocalStorageByAttribute('accessTokenDetails');
     console.log('token', token);
     console.log('isTokenExpired', jwtService.isTokenExpired(token.token));
   } catch (error) {
@@ -70,10 +72,11 @@ export const InterceptService: HttpInterceptorFn = (req, next) => {
 
   // Check if the token is expired
   if (token && jwtService.isTokenExpired(token.token)) {
-    // const data = { "userName": token.user.userName, "token": token.refreshToken }
+    // const accessTokenDetails = storageService.getLocalStorageByAttribute('accessTokenDetails');
+    const data = { "userName": token.user.userName, "token": token.refreshToken }
     console.log('Token is expired. Logging out...');
-    // authService.logout(data); // Logout the user
-    // router.navigate(['/landing-page/signin']); // Redirect to login page
+    authService.logout(data); // Logout the user
+    router.navigate(['/landing-page/signin']); // Redirect to login page
     // return throwError('Token expired'); // Stop the request
   }
 
@@ -126,122 +129,8 @@ export const InterceptService: HttpInterceptorFn = (req, next) => {
         }
       }
 
-      // if (errorHandled) {
-      //   return throwError(error); // Return the error to the subscriber
-      // } else {
-      //   return throwError(error); // Propagate the error to the subscriber
-      // }
-      // return throwError(error);
       return errorHandled ? throwError(error) : throwError(error);
     })
   );
 };
-
-// export const InterceptService: HttpInterceptorFn = (req, next) => {
-//   // try {
-//   //   const jwtService = inject(JwtHelperService);  // Attempt to inject JwtHelperService
-//   //   console.log('after jwtservice');
-//   //   console.log('jwtService', jwtService);
-//   // } catch (error) {
-//   //   console.error('Error injecting JwtHelperService:', error);  // Log any injection errors
-//   // }
-//   const jwtService = inject(JwtHelperService);
-//   const router = inject(Router);
-//   const authService = inject(AuthService);
-//   let ifErrorHandled: boolean = false;
-
-
-//   const token: any = localStorage.getItem('token');  // Or use a service to get token
-//   // const data = { "userName": token.user.userName, "token": token.refreshToken }
-
-//   // Check if the token is expired
-//   // if (token && jwtService.isTokenExpired(token)) {
-//   //   console.log('token', token);
-//   //   console.log('isTokenExpired', jwtService.isTokenExpired(token));
-//   //   const data = { "userName": token.user.userName, "token": token.refreshToken }
-//   //   authService.logout(data);  // Logout the user
-//   //   router.navigate(['/login']);  // Redirect to login page
-//   //   return throwError('Token expired');
-//   // }
-//   setInterval(() => {
-//     if (token && jwtService.isTokenExpired(token)) {
-//       console.log('token', token);
-//       console.log('isTokenExpired', jwtService.isTokenExpired(token));
-//       const data = { "userName": token.user.userName, "token": token.refreshToken }
-//       authService.logout(data);  // Logout the user
-//       router.navigate(['/login']);  // Redirect to login page
-//       // return throwError('Token expired');
-//     }
-//   }, 60000);
-//   return next(req).pipe(
-//     retryWhen(error =>
-//       error.pipe(
-//         concatMap((error, count) => {
-//           console.warn('interceptor concatMap');
-
-//           if (count < retryCount) {
-//             if (error.status == 401) {
-//               console.warn('interceptor retry');
-//               return of(error); //  'returns back' from here and try again with out throwing error and continuing next line.
-//               // can handel refresh token here.
-//             }
-//           }
-//           return throwError(error); // throws error to the catchError
-//         }),
-//         delay(retryDelayMS),
-//       )
-//     ),
-//     map((event: HttpEvent<any>) => {
-//       return event;
-//     }),
-//     catchError((error: HttpErrorResponse) => {
-//       console.error('Logging Interceptor Functional Error:', error);
-//       if (error instanceof HttpErrorResponse) {
-//         console.warn('catchError HttpErrorResponse', error.error, error.status, error.statusText, error.type);
-//         if (error.error instanceof ErrorEvent) {
-//           console.error("catchError Error Event");
-//         } else {
-//           switch (error.status) {
-//             case 401:  // login
-//               router.navigate(['/landing-page/login'], { queryParams: errorMsgs[error.status] });
-//               ifErrorHandled = true;
-//               break;
-//             case 403: // forbidden
-//               router.navigate(['/landing-page/login'], { queryParams: errorMsgs[error.status] }); // window.location.href = "index.html#/error";
-//               ifErrorHandled = true;
-//               break;
-//             case 500: // internal server error
-//               router.navigate(['/error'], { queryParams: errorMsgs[error.status] });
-//               ifErrorHandled = true;
-//               break;
-//             case 404: // page not found
-//               router.navigate(['/error'], { queryParams: errorMsgs[error.status] });
-//               ifErrorHandled = true;
-//               break;
-//             case 400: // bad request
-//               // this.router.navigate(['/error'], { queryParams: errorMsgs[error.status] });
-//               // this.ifErrorHandled = true;
-//               break;
-//           }
-//         }
-//       } else {
-//         console.error("Other Errors");
-//       }
-
-//       if (ifErrorHandled) {
-//         console.log('return back');
-//         // return of(error);
-//         return throwError(error);
-//       } else {
-//         console.log('throw error back to the subscriber');
-//         return throwError(error);
-//         // this.clientErrorHandlerService.handleError(error);
-//       }
-//       // return throwError(error);
-//     })
-//   );
-
-//   // return next(req);
-// }
-
 
